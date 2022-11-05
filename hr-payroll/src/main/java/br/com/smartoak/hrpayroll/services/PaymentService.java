e42344a13c9b779a0650b8bf9ec5c9746a97fbac
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-11-05 16:31:42 -0300

    Feign client

--- hr-payroll/src/main/java/br/com/smartoak/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/smartoak/hrpayroll/services/PaymentService.java
@@ -1,30 +1,21 @@
 package br.com.smartoak.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.smartoak.hrpayroll.entities.Payment;
 import br.com.smartoak.hrpayroll.entities.Worker;
+import br.com.smartoak.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", "" + workerId);
 		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
