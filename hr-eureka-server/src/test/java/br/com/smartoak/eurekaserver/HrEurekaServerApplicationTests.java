09385cbc4075247b827c1f5637b206ea2c68c2e5
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-11-05 17:07:16 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/smartoak/eurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartoak.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
