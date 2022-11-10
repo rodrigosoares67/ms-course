620477002fc4c8f0690e146df08abdb125d51ef7
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-11-09 21:58:04 -0300

    User, Role, many to many association

--- hr-user/src/main/java/br/com/smartoak/hruser/entities/User.java
+++ hr-user/src/main/java/br/com/smartoak/hruser/entities/User.java
@@ -6,9 +6,11 @@ import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
@@ -26,9 +28,12 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	
-	@ManyToMany
-	@JoinTable(name = "tb_user_role")
-	Set<Role> roles = new HashSet<>();
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 		
@@ -77,6 +82,10 @@ public class User implements Serializable {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
 	}
 
 	@Override
