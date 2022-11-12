6de073aec3ca57b6665dd4d85777c63dbdeaeaab
Rodrigo Soares <rodrigo.soares67@outlook.com>
2022-11-12 15:59:30 -0300

    Login, JWTtoken finish

--- hr-oauth/src/main/java/br/com/smartoak/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/smartoak/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.smartoak.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.smartoak.hroauth.entities.User;
 import br.com.smartoak.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -25,6 +28,20 @@ public class UserService {
 		}
 		
 		logger.info("E-mail found: " + email);
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if(user == null) {
+			logger.error("E-mail not found: " + username);
+			throw new UsernameNotFoundException("E-mail not found");
+		}
+		
+		logger.info("E-mail found: " + username);
 		
 		return user;
 	}
